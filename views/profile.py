9d8f578e455472c63b9fb9b80f968571ca1ea198
DarkShadowG17 <rouissiyazh@gmail.com>
2024-06-09 16:50:02 +0100

    handle profile bug

--- views/profile.py
+++ views/profile.py
@@ -1,4 +1,4 @@
-from flask import render_template,session,redirect
+from flask import render_template,session,redirect,abort
 from views  import app_views
 @app_views.route("/profile",strict_slashes=False)
 def profile():
@@ -6,4 +6,4 @@ def profile():
         if session['logged']:
             return render_template("accounts/profile.html",fullname=session['nom']+' '+session['prenom'],nom=session['nom'],prenom=session['prenom'],email=session['email'],lastLogin=session['lastLogin'],creationDate=session['creationDate'])
         return redirect('/',code=303)
-    return 'unauthorized'
+    return abort(401)
