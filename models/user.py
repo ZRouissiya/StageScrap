73d83d2f1e3a7adf122dbb39d12107bfcfe962ab
DarkShadowG17 <rouissiyazh@gmail.com>
2024-06-13 11:27:12 +0100

    switched from session based to user class based with major updates

--- models/user.py
+++ models/user.py
@@ -1,40 +1,80 @@
 import mysql.connector as con
 from flask_bcrypt import Bcrypt
+import datetime
 db=con.connect(host="localhost",user="root",password="",database='db_stage')
 cursor=db.cursor()
 bc=Bcrypt()
 
-class user():
-    def __init__(self,name,lastName,email,password,userId=None):
+class User():
+    def __init__(self,name,lastName,email,password,createDate=datetime.datetime.today().strftime('%d-%m-%Y'),lastLogin=datetime.datetime.today().strftime('%d-%m-%Y'),userId=None):
         self.name=name
         self.lastName=lastName
         self.email=email
         self.password=password
-        if userId is None:
-            self.userId=self.insert(name,lastName,email,password)
-
-
-    def insert(self,name,lastName,email,password):
-        hashedPwd=bc.generate_password_hash(password).decode("utf-8")
-        sql="INSERT INTO user (email,password) VALUES (%s,%s)"
-        val=(email,hashedPwd)
-        cursor.execute(sql,val)
-        id=cursor.lastrowid
-        sql2="INSERT INTO userdata (userId,nom,prenom,creationDate,lastLogin) VALUES (%s,%s,%s,CURDATE(),CURDATE())"
-        val2=(int(id),lastName,name)
-        cursor.execute(sql2,val2)
-        db.commit()
-        return id
-     
+        self.createDate=createDate
+        self.lastLogin=lastLogin
+        self.userId=userId
+    def fullname(self):
+        return self.name +' '+self.lastName
+    def insert(self):
+        try:
+            hashedPwd=bc.generate_password_hash(self.password).decode("utf-8")
+            sql="INSERT INTO user (email,password) VALUES (%s,%s)"
+            val=(self.email,hashedPwd)
+            cursor.execute(sql,val)
+            id=cursor.lastrowid
+            sql2="INSERT INTO userdata (userId,nom,prenom,creationDate,lastLogin) VALUES (%s,%s,%s,CURDATE(),CURDATE())"
+            val2=(int(id),self.lastName,self.name)
+            cursor.execute(sql2,val2)
+            db.commit()
+            self.userId=id
+            return True
+        except Exception as e:
+            return False
+        
     def updateData(self,name,lastName,email):
         cursor.execute(f"SELECT * FROM user WHERE email='{email}'")
         rs=cursor.fetchall()
+        if len(rs)==1:
+            if self.email==rs[0][1]:
+                if self.updateToDb(name,lastName,email):
+                    
+                    return 1
+                return -1
+            else:
+                return 0
         if email!=self.email and len(rs)==0:
-            try:
+            self.updateToDb(name,lastName,email)
+            return 1
+        return -1
+
+    
+    def updatePassword(self,newPassword):
+        try:
+            hashedPwd=bc.generate_password_hash(newPassword).decode('utf-8')
+            sql=f"UPDATE user SET password='{hashedPwd}' where email='{self.email}'"
+            cursor.execute(sql)
+            db.commit()
+            return True
+        except Exception as e:
+            return False   
+        
+          
+    def logOut(self):
+        try:
+            cursor.execute(f"UPDATE `userdata` SET `lastLogin`=CURDATE() WHERE userId = (SELECT userId from user where email='{self.email}')")
+            db.commit()
+            del self
+            return True
+        except Exception as ex:
+            return False
+        
+    def updateToDb(self,name,lastName,email):
+        try:
                 cursor.execute(f"UPDATE `user` SET `email`='{email}' WHERE email='{self.email}'")
                 self.email=email
-                if len(lastName)==0 and len(name)==0 :
-                    sql=f"UPDATE `userdata` SET `nom`='{lastName}',`prenom`='{name}' WHERE userId = {self.userId}')"
+                if len(lastName)!=0 and len(name)!=0 :
+                    sql=f"UPDATE `userdata` SET `nom`='{lastName}',`prenom`='{name}' WHERE userId = {self.userId}"
                     cursor.execute(sql)
                     self.name=name
                     self.lastName=lastName
@@ -42,15 +82,5 @@ class user():
                     db.commit()
                     return True
                 return False
-            except Exception as e:
-                return False
-
-    
-    def updatePassword(self,newPassword):
-        hashedPwd=bc.generate_password_hash(newPassword).decode('utf-8')
-        sql=f"UPDATE user SET password='{hashedPwd}' where email='{self.email}'"
-        cursor.execute(sql)
-        db.commit()
-     
-    def logOut(self):
-        del self
+        except Exception as e:
+            return False
--- views/dashboard.py
+++ views/dashboard.py
@@ -2,14 +2,17 @@ from flask import render_template,session,redirect
 from views import app_views
 from models.scrapStagiaires import  scrap
 from models.scrapMAStage import MAStage
+import pickle
 @app_views.route("/dashboard",strict_slashes=False)
 def dashboard():
-    if session.get('logged'):
+    if "user" in session:
         scr=scrap()
         demandes=scr.demandes(numPage=3)
         session['stagiaires']=demandes
         scr=MAStage()
         demandes=scr.scrapData(numPage=2)
         session['marocAnnonces']=demandes
-        return render_template("dashboard.html",email=session['email'],Stagiaires=session['stagiaires'],MarocAnnonces=session['marocAnnonces'])
+        currentUser=session.get('user')
+        user=pickle.loads(currentUser)
+        return render_template("dashboard.html",email=user.email,Stagiaires=session['stagiaires'],MarocAnnonces=session['marocAnnonces'])
     return redirect("/")
